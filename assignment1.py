da69aac33d2a75abcece19c07819f7330e918409
audryaa <audryabonyo@gmail.com>
2026-07-21 17:19:32 -0700

    add assignment1.py script

---
+++ assignment1.py
@@ -0,0 +1,48 @@
+first_name = "Audry"
+last_name = "Abonyo"
+age = 19
+height = "172cm"
+course = "computer science"
+university = "Riara University"
+year_of_Study = 1
+favorite_programming_language = "Python"
+dream_company = "Microsoft"
+dream_job = "Data Science"
+number_of_siblings = 3
+owns_a_laptop = True
+has_programming_experience = False
+
+#printing information 
+
+print("==============STUDENT PROFILE ==============")
+print(f"Name: {first_name} {last_name}")
+print(f"Age: {age}")
+print(f"Height: {height}")
+print(f"Course: {course}")
+print(f"University: {university}")
+print(f"Year: {year_of_Study}")
+
+print()
+print(f"Favourite Language: {favorite_programming_language}")
+print(f"Dream Job: {dream_job}")
+print(f"Dream Company: {dream_company}")
+
+print()
+print(f"Owns Laptop: {owns_a_laptop}")
+print(f"Programming Experience: {has_programming_experience}")
+print()
+
+# displaying data types of variables created
+
+print(type(first_name))
+print(type(last_name))
+print(type(age))
+print(type(height))
+print(type(course))
+print(type(university))
+print(type(year_of_Study))
+print(type(favorite_programming_language))
+print(type(dream_job))
+print(type(dream_company))
+print(type(number_of_siblings))
+print(type(owns_a_laptop))
