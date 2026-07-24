c39f7b0b913040114de4245f7d95a1deb4326ddd
audryaa <audryabonyo@gmail.com>
2026-07-23 18:22:59 -0700

    grade calculator

---
+++ gradecalculator.py
@@ -0,0 +1,17 @@
+marks = int(input("Enter your marks: "))
+
+if marks >= 80:
+    print("You scored an A ")
+
+elif marks >= 70:
+    print("You scored a B")
+
+elif marks >= 50:
+    print("You scored a C")
+
+elif marks >= 40:
+    print("You scored a D")
+
+else:
+    print("You got a F")
+    
